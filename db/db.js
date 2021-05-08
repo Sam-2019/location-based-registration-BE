bdad9f2d6f90b4072b6be34e9ad00e5f16f22249
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-08 02:29:47 +0000

    added cloud dB

--- db/db.js
+++ db/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/dataset", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb://localhost:27017/dataset", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-// module.exports = {
-//   mongoURI: `mongodb+srv://${rocess.env.DB_USER}:${DB_APIKEY}@cluster0.dottv.mongodb.net/${DB_COLLECTION}?retryWrites=true&w=majority`,
-// };
+module.exports = {
+  mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.envDB_APIKEY}@cluster0.dottv.mongodb.net/${process.envDB_COLLECTION}?retryWrites=true&w=majority`,
+};
 
 var dbConn = mongoose.connection;
 dbConn.on("connected", function () {
